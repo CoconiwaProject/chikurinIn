bb347d1810757a240af0c63ac851d2d2042f1613
LAPTOP-MCNSDPCJ\g1527 <g1527066@st.t-kougei.ac.jp>
2019-02-17 17:52:25 +0900

    コンテンツ文更新

--- CoconiwaChikurinin/Assets/Editor/JSONImpoter.cs
+++ CoconiwaChikurinin/Assets/Editor/JSONImpoter.cs
@@ -8,8 +8,8 @@ public class JSONImpoter : AssetPostprocessor
 
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
     {
-        string targetFile = "Assets/CSV/ChinaSimplification.json";
-        string exportFile = "Assets/Resources/ContentsData/Chinese.asset";
+        string targetFile = "Assets/CSV/English.json";
+        string exportFile = "Assets/Resources/ContentsData/English.asset";
 
         foreach (string asset in importedAssets)
         {
