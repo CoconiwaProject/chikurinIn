17de491ad34da048d73ddd556483f1b3b0469264
LAPTOP-MCNSDPCJ\g1527 <g1527066@st.t-kougei.ac.jp>
2019-03-06 03:52:49 +0900

    コンテンツリスト修正

--- CoconiwaChikurinin/Assets/Scripts/ContentList/ContentListManager.cs
+++ CoconiwaChikurinin/Assets/Scripts/ContentList/ContentListManager.cs
@@ -52,12 +52,29 @@ public class ContentListManager : MonoBehaviour
         ContentGroupN.Create();
         ContentGroupH.Create();
         ContentGroupY.Create(ContentGroupN.mostUnderItem);
+        RectTransform hRect = ContentGroupH.transform as RectTransform;
+        RectTransform nRect= ContentGroupN.transform as RectTransform;
+        RectTransform yRect = ContentGroupY.transform as RectTransform;
+
+
+        float iconSize = 100;
+        //一つ上のグループのYからアイコンサイズ、グループ下の座標、Yサイズを引く、めんどいので関数化してない
+        RectTransform hUnderItemRect = ContentGroupH.mostUnderItem.transform as RectTransform;
+        float nY = hRect.anchoredPosition.y - iconSize + hUnderItemRect.anchoredPosition.y - hUnderItemRect.sizeDelta.y *hUnderItemRect.localScale.y;
+        nRect.anchoredPosition = new Vector2(nRect.anchoredPosition.x, nY);
+
+
+        RectTransform nUnderItemRect = ContentGroupN.mostUnderItem.transform as RectTransform;
+        float yY = nRect.anchoredPosition.y - iconSize + nUnderItemRect.anchoredPosition.y 
+            - nUnderItemRect.sizeDelta.y * nUnderItemRect.localScale.y;
+        yRect.anchoredPosition = new Vector2(yRect.anchoredPosition.x, yY);
+
 
         RectTransform rec = ContentGroupY.mostUnderItem.transform as RectTransform;
         RectTransform parentRec = rec.parent as RectTransform;
         float limit = rec.anchoredPosition.y + parentRec.anchoredPosition.y - 100.0f;
         Vector2 contentRecSize = contentRec.sizeDelta;
-        contentRecSize.y = Mathf.Abs(limit)*2;
+        contentRecSize.y = Mathf.Abs(limit);
         contentRec.sizeDelta = contentRecSize;
     }
 
